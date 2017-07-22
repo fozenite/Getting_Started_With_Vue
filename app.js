f565c47f99d8c85149077b1d8a9eeadc60ce8576
Rohit <moza.rohit@gmail.com>
2017-07-22 13:48:27 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,71 @@
+
+// Exercise 1
+// new Vue({
+//   el: '#app',
+//   data: {
+//     title: 'Hello World!',
+//     link: 'http://google.com',
+//     finishedLink: '<a href="http://google.com">Google</a>'
+//   },
+//   methods: {
+//     sayHello: function() {
+//       this.title = 'Hello';
+//       return this.title;
+//     }
+//   }
+
+// });
+
+// Exercise 2 - Assignment Type
+// new Vue ({
+//   el: '#exercise',
+//   data: {
+//     name: 'Rohit',
+//     age : '27',
+//     image: 'http://www.familiesmattercoop.ca/files/familiesmattercoop/node-images/sunshine_.jpg'
+//   },
+//   methods: {
+//     randomNum: function() {
+//       min = Math.ceil(0);
+//       max = Math.floor(1);
+//       return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+//     }
+//   }
+// })
+
+// Exercise 3
+// new Vue ({
+//   el: '#app',
+//   data: {
+//     counter: 0,
+//     x: 0,
+//     y: 0
+//   },
+//   methods: {
+//     increase: function(step, event) {
+//       this.counter += step;
+//     },
+//     updateCoordinates: function(event) {
+//       this.x = event.clientX;
+//       this.y = event.clientY;
+//     },
+//     alertMe: function() {
+//       alert('Alert!');
+//     }
+
+//   }
+// })
+  // Exercise 4 -
+new Vue ({
+  el: '#exercise',
+  data: {
+    value: '';
+  },
+  methods: {
+
+
+  }
+})
+
+
+
