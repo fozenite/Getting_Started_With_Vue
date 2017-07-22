2c457c9f8f2404091ace11b29defc0b6bf227cea
Rohit <moza.rohit@gmail.com>
2017-07-22 19:14:40 -0400

    Using shorthands

--- app.js
+++ app.js
@@ -131,3 +131,16 @@
 //   }
 
 // })
+
+//Exercise 8 - Using Shorthands
+// new Vue({
+//   el: '#app',
+//   data: {
+//     link: 'http://google.com'
+//   },
+//   methods: {
+//     changeLink: function() {
+//       this.link = 'http://apple.com'
+//     }
+//   }
+// });
