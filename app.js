49eb6a4b55ef4adb108d39e2da259ed8a20b957d
Rohit <moza.rohit@gmail.com>
2017-07-22 20:50:46 -0400

    Inline Styling

--- app.js
+++ app.js
@@ -189,3 +189,20 @@
 //     }
 //   }
 // });
+
+// Exercise 11 - Inline Styling
+// new Vue({
+//   el: '#app',
+//   data: {
+//     color: 'gray',
+//     width: 100
+//   },
+//   computed: {
+//     myStyle: function() {
+//       return {
+//         backgroundColor: this.color,
+//         width: this.width + 'px'
+//       }
+//     }
+//   }
+// });
