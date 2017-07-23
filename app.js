08e9f1a75f3329b7baa17d242cf9702ff06269da
Rohit <moza.rohit@gmail.com>
2017-07-23 14:00:15 -0400

    Conditional Rendering Assignment Completed

--- app.js
+++ app.js
@@ -268,13 +268,32 @@
 // });
 
 // Exercise 14 - Conditional Rendering with v-for
+// new Vue({
+//   el: '#app',
+//   data: {
+//     ingredients: ['meat', 'fruit', 'cookies'],
+//     persons: [
+//       {name: 'Max', age: 27, color: 'red'},
+//       {name: 'Anna', age: 'unknown', color: 'blue'}
+//     ]
+//   }
+// });
+
+// Exercise 15 - Conditonal Rendering Assignment
 new Vue({
-  el: '#app',
+  el: '#exercise',
   data: {
-    ingredients: ['meat', 'fruit', 'cookies'],
-    persons: [
-      {name: 'Max', age: 27, color: 'red'},
-      {name: 'Anna', age: 'unknown', color: 'blue'}
-    ]
+    show: true,
+    array: ['Max', 'Anna', 'Chris', 'Manu'],
+    myObject: {
+      title: 'Lord of the Rings',
+      author: 'J.R.R. Tolkiens',
+      books: '3'
+    },
+    testData: {
+      name: 'TESTOBJECT',
+      id: 10,
+      data: [1.67, 1.33, 0.98, 2.21]
+    }
   }
 });
