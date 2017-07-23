2f3a46d91388a9df5b953888842f847f69450046
Rohit <moza.rohit@gmail.com>
2017-07-22 20:30:47 -0400

    Worked with CSS classes and controlled via computed functions

--- app.js
+++ app.js
@@ -172,3 +172,20 @@
 //     }
 //   }
 // })
+
+// Exercise 10 - CSS Classes
+// new Vue({
+//   el: '#app',
+//   data: {
+//     attachRed: false,
+//     color: 'green'
+//   },
+//   computed: {
+//     divClasses: function(){
+//       return {
+//         red: this.attachRed,
+//         blue: !this.attachRed
+//       };
+//     }
+//   }
+// });
